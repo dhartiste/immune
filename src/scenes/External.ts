5981757eef4662cf4f4b4770c4a289b61e920e13
erugg22 <41970889+erugg22@users.noreply.github.com>
2021-01-11 17:19:02 -0500

    Currently trying to get gotoAndStop() to work in External.ts

--- src/scenes/External.ts
+++ src/scenes/External.ts
@@ -1,28 +1,30 @@
 import { MovieClip } from 'pixi-animate';
 import { Scene, AssetList, PauseableTimer } from 'wgbh-springroll-game';
-import * as GameArt from '../assets/External';
+import * as ExternalArt from '../assets/External';
 
 export default class GameScene extends Scene {
     
-    private art: Art;
+    private art: ExternalArt;
     
     preload():AssetList{
         return [
-            {type:'animate', id:'gameArt', stage:GameArt.stage, cacheInstance:true},
+            {type:'animate', id:'externalArt', stage:ExternalArt.stage, cacheInstance:true},
         ];
     }
 
     setup(){
-        this.art = this.cache.animations.gameArt as Art;
-        this.art.patient.gotoAndStop(0);
-        //this.art.btnAhead.gotoAndStop(0);
-        //this.art.btnBack.gotoAndStop(0);
+        this.art = this.cache.animations.externalArt as ExternalArt;
         this.addChild(this.art);
+
+        this.art.patient.gotoAndStop(0);
     }
 
     start(){
-        this.interactive = true;
-        this.interactiveChildren = true;
+        // to do
+    }
+
+    endAnimation = () => {
+        //PIXI.animate.Animator.play(this.art.patient, "sickToSickInBed");
     }
 
     update(){
@@ -34,13 +36,15 @@ export default class GameScene extends Scene {
     }
 }
 
-interface Art extends PIXI.animate.MovieClip {
-    patient:PIXI.animate.MovieClip;
+interface ExternalArt extends MovieClip {
+    patient: MovieClip;
+    /*
     dTempFC:PIXI.animate.MovieClip;
     Health:PIXI.animate.MovieClip;
     bandaid:PIXI.animate.MovieClip;
     Protection:PIXI.animate.MovieClip;
     dDescription:PIXI.animate.MovieClip;
-    //btnAhead:PIXI.animate.MovieClip;
+    */
+    //btnAhead:PIXI.animate.button;
     //btnBack:PIXI.animate.MovieClip;
 }
